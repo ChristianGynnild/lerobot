b3c84e306147fbd2e4d8352ab14837883a17999c
christiangynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-10-28 16:28:41 +0100

    Revert "Include option to not save episode"
    This reverts commit b3ce13c3582021cae2eea5de210ef1523e6a48a2.

--- src/lerobot/record.py
+++ src/lerobot/record.py
@@ -367,21 +367,15 @@ def record(cfg: RecordConfig) -> LeRobotDataset:
                     display_data=cfg.display_data,
                 )
 
+            if events["rerecord_episode"]:
+                log_say("Re-record episode", cfg.play_sounds)
+                events["rerecord_episode"] = False
+                events["exit_early"] = False
+                dataset.clear_episode_buffer()
+                continue
 
-            while True:
-                reply = input("Save episode? (y/n):")
-                if reply == "y":
-                    dataset.save_episode()
-                    recorded_episodes += 1
-                    break
-                if reply == "n":
-                    log_say("Re-record episode", cfg.play_sounds)
-                    events["rerecord_episode"] = False
-                    events["exit_early"] = False
-                    dataset.clear_episode_buffer()
-                    break
-                else:
-                    print("Invalid input. Please input y or n.")            
+            dataset.save_episode()
+            recorded_episodes += 1
 
     log_say("Stop recording", cfg.play_sounds, blocking=True)
 
